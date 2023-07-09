a430190cae9de15b249e3c8214fe441e3fbe96dd
Guillermo Olmos <geordev2@gmail.com>
2023-07-09 20:24:52 +0100

    Integrating emotion to project [sc-39]

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
 import 'tailwindcss/tailwind.css';
+import { jsx, css } from '@emotion/react';
 import './App.css';
 
 import logo from './assets/images/logo_100x100.png';
 
 function App() {
+  const containerStyles = css`
+    background-color: #e2e8f0;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const cardStyles = css`
+    max-width: md;
+    padding: 6;
+    background-color: white;
+    border-radius: lg;
+    box-shadow: lg;
+    display: flex;
+    align-items: center;
+  `;
+
+  const logoStyles = css`
+    width: 16px;
+    height: 16px;
+    margin-right: 4;
+  `;
+
+  const titleStyles = css`
+    font-size: 2xl;
+    font-weight: bold;
+    margin-bottom: 4;
+  `;
+
+  const descriptionStyles = css`
+    color: #4a5568;
+  `;
+
+  const buttonStyles = css`
+    margin-top: 4;
+    padding: 2 4;
+    background-color: #3b82f6;
+    color: white;
+    border-radius: rounded;
+    &:hover {
+      background-color: #2563eb;
+    }
+  `;
+
   return (
-    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
-      <div className="max-w-md p-6 bg-white rounded-lg shadow-lg flex items-center">
-        <img src={logo} alt="Logo" className="w-16 h-16 mr-4" />
+    <div css={containerStyles}>
+      <div css={cardStyles}>
+        <img src={logo} alt="Logo" css={logoStyles} />
         <div>
-          <h1 className="text-2xl font-bold mb-4">
-            <span className="inline-block align-middle">Welcome to Agile Biscuits App</span>
+          <h1 css={titleStyles}>
+            <span css={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              Welcome to Agile Biscuits App
+            </span>
           </h1>
-          <p className="text-gray-700">
+          <p css={descriptionStyles}>
             This is a sample application using Tailwind CSS.
           </p>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <button css={buttonStyles}>
             Click Me
           </button>
         </div>
