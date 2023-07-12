82606034caed41a2bc09763b67edb9ccc3170642
smilee <smilee@users.noreply.github.com>
2023-07-13 03:53:29 +0900

    Show favicon (#5)
    * Show favicon
    
    - Add favicon
    - Add manifest.json

--- fixtures/budgets.js
+++ fixtures/budgets.js
@@ -4,17 +4,17 @@ const budgets = [
   new Budget({
     name: 'Food',
     amount: 100,
-    value: 80,
+    value: 80.10,
   }),
   new Budget({
     name: 'Shopping',
     amount: 200,
-    value: 224,
+    value: 572293284,
   }),
   new Budget({
     name: 'Utilities',
     amount: 100,
-    value: 36,
+    value: 36.29,
   }),
   new Budget({
     name: 'Hobbies',
