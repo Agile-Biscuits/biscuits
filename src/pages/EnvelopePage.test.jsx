bbd09eb1051f8ad653fafb3e14543bec6712181e
Guillermo Olmos <geordev2@gmail.com>
2023-07-15 17:36:34 +0100

    Deleting budget from the /envelope screen [sc-53]
    - test passing
    - trash can icon next to budget name

--- src/pages/EnvelopePage.test.jsx
+++ src/pages/EnvelopePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import EnvelopePage from "./EnvelopePage";
 import { BudgetsContext } from '../context/BudgetsContext';
 
@@ -19,5 +19,29 @@ describe("EnvelopePage", () => {
     expect(budgetNames[0]).toHaveTextContent("Groceries");
     expect(budgetNames[1]).toHaveTextContent("Rent");
     expect(budgetNames.length).toBe(2);
+  });
+
+  it("deletes a budget when the trash can icon is clicked", async () => {
+    const budgets = [
+      { id: 1, name: "Groceries" },
+      { id: 2, name: "Rent" },
+    ];
+
+    // Create a mock function
+    const setBudgets = jest.fn();
+
+    const { getAllByTestId, queryByText } = render(
+      <BudgetsContext.Provider value={{ budgets, setBudgets }}>
+        <EnvelopePage />
+      </BudgetsContext.Provider>
+    );
+
+    const trashIcons = getAllByTestId("trash-icon");
+    fireEvent.click(trashIcons[0]);
+
+    // Check that setBudgets was called with the correct arguments
+    expect(setBudgets).toHaveBeenCalledWith([
+      { id: 2, name: "Rent" },
+    ]);
   });
 });
